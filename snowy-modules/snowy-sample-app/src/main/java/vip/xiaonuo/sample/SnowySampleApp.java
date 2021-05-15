716379adca952e5168be6ea3cdc6a6ca652f1321
dongxiayu <chinadongxiayu@163.com>
2021-05-15 10:17:31 +0800

    【更新】sample实现调整，基础准备

--- snowy-modules/snowy-sample-app/src/main/java/vip/xiaonuo/sample/SnowySampleApp.java
+++ snowy-modules/snowy-sample-app/src/main/java/vip/xiaonuo/sample/SnowySampleApp.java
@@ -27,6 +27,7 @@ package vip.xiaonuo.sample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * SpringBoot方式启动类
@@ -34,6 +35,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * @author yubaoshan
  * @date 2017/5/21 12:06
  */
+@EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages = {"vip.xiaonuo"})
 public class SnowySampleApp {
--- snowy-modules/snowy-sample-app/src/main/java/vip/xiaonuo/sample/config/CacheConfig.java
+++ snowy-modules/snowy-sample-app/src/main/java/vip/xiaonuo/sample/config/CacheConfig.java
@@ -26,10 +26,16 @@ package vip.xiaonuo.sample.config;
 
 import cn.hutool.cache.CacheUtil;
 import cn.hutool.cache.impl.TimedCache;
-import vip.xiaonuo.sample.cache.MappingCache;
-import vip.xiaonuo.sample.cache.ResourceCache;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import vip.xiaonuo.cache.MappingCache;
+import vip.xiaonuo.cache.ResourceCache;
+import vip.xiaonuo.cache.UserCache;
+import vip.xiaonuo.common.pojo.login.SysLoginUser;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import vip.xiaonuo.sample.core.redis.FastJson2JsonRedisSerializer;
 
 import java.util.Map;
 
@@ -55,20 +61,57 @@ public class CacheConfig {
 
 
     /**
+     * redisTemplate 配置
+     *
+     * @author dongxiayu
+     * @date 2021/1/23 13:55
+     */
+    @Bean
+    public RedisTemplate<String, SysLoginUser> userRedisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, SysLoginUser> userRedisTemplate = new RedisTemplate<>();
+        userRedisTemplate.setConnectionFactory(factory);
+        userRedisTemplate.setKeySerializer(new StringRedisSerializer());
+        userRedisTemplate.setValueSerializer(new FastJson2JsonRedisSerializer<>(SysLoginUser.class));
+        userRedisTemplate.afterPropertiesSet();
+        return userRedisTemplate;
+    }
+
+    /**
+     * redisTemplate 配置
+     *
+     * @author dongxiayu
+     * @date 2021/1/23 13:55
+     */
+    @Bean
+    public RedisTemplate<String, Map<String, Object>> mappingRedisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Map<String, Object>> mappingRedisTemplate = new RedisTemplate<>();
+        mappingRedisTemplate.setConnectionFactory(factory);
+        mappingRedisTemplate.setKeySerializer(new StringRedisSerializer());
+        mappingRedisTemplate.setValueSerializer(new FastJson2JsonRedisSerializer<>(Map.class));
+        mappingRedisTemplate.afterPropertiesSet();
+        return mappingRedisTemplate;
+    }
+
+    /**
+     * 登录用户的缓存，默认过期时间，根据系统sys_config中的常量决定
+     *
+     * @author yubaoshan
+     * @date 2020/7/9 11:44
+     */
+    @Bean
+    public UserCache userCache(RedisTemplate<String, SysLoginUser> userRedisTemplate) {
+        return new UserCache(userRedisTemplate);
+    }
+
+    /**
      * mapping映射缓存
      *
      * @author xuyuxiang
      * @date 2020/7/24 13:55
      */
     @Bean
-    public MappingCache mappingCache() {
-        TimedCache<String, Map<String, Object>> timedCache =
-                CacheUtil.newTimedCache(2 * 60 * 1000);
-
-        // 定时清理缓存，间隔1秒
-        timedCache.schedulePrune(1000);
-
-        return new MappingCache(timedCache);
+    public MappingCache mappingCache(RedisTemplate<String, Map<String, Object>> mappingRedisTemplate) {
+        return new MappingCache(mappingRedisTemplate);
     }
 
 }
