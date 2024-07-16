9312ea9f7ec3ff06a22392d3d57f531f046019f4
dongxiayu <chinadongxiayu@163.com>
2024-07-16 23:58:03 +0800

    【修复】StpInterfaceImpl.getPermissionList性能问题处理-ALL SNOWY/CLOUD

--- snowy-base/snowy-common/src/main/java/vip/xiaonuo/common/consts/CacheConstant.java
+++ snowy-base/snowy-common/src/main/java/vip/xiaonuo/common/consts/CacheConstant.java
@@ -24,4 +24,14 @@ public class CacheConstant {
      */
     public static final String PERMISSION_RESOURCE_CACHE_KEY = "permission-resource";
 
+    /**
+     * B端权限列表
+     */
+    public static final String AUTH_B_PERMISSION_LIST_CACHE_KEY = "auth-b-permission-list:";
+
+    /**
+     * C端权限列表
+     */
+    public static final String AUTH_C_PERMISSION_LIST_CACHE_KEY = "auth-c-permission-list:";
+
 }
